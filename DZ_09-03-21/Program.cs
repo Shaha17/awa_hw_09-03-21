b86ca462a8f76a32a0d12615c03ed765203a191b
ShahaKB <shahzodjabbarov@gmail.com>
2021-03-10 21:23:24 +0500

    add MyDictionary

--- DZ_09-03-21/Program.cs
+++ DZ_09-03-21/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.ComponentModel;
+using System;
 
 namespace DZ_09_03_21
 {
@@ -10,11 +11,21 @@ namespace DZ_09_03_21
 			MyList<int> lst = new MyList<int>();
 			lst.AddRange(1, 2, 3, 4, 45, 6, 6);
 			lst.Show();
+
+
+			System.Console.WriteLine();
+
+			MyDictionary<int, string> dict = new MyDictionary<int, string>();
+			dict.Add(1, "1");
+			dict.Add(2, "2");
+			dict.Add(3, "3");
+			dict.Add(4, "4");
+			dict.Show();
 		}
 	}
 	class MyDictionary<TKey, TValue>
 	{
-        TValue[] values;
+		TValue[] values;
 		TKey[] keys;
 
 		public int Count
@@ -24,95 +35,125 @@ namespace DZ_09_03_21
 				return values.Length;
 			}
 		}
-		public MyList()
+		public MyDictionary()
 		{
 			this.keys = new TKey[0];
 			this.values = new TValue[0];
 		}
-		public MyList(params T[] itemsToAdd)
+		public int RemoveAt(TKey toRemove)
 		{
-			this.arr = new T[itemsToAdd.Length];
-			for (int i = 0; i < this.arr.Length; i++)
+			int iToRm = -1;
+			for (int i = 0; i < this.keys.Length; i++)
 			{
-				this.arr[i] = itemsToAdd[i];
+				if (this.keys[i].Equals(toRemove))
+				{
+					iToRm = i;
+				}
 			}
-		}
-		public int RemoveAt(int iToRemove)
-		{
-			if (this.arr.Length <= iToRemove)
+			if (iToRm == -1)
 			{
 				throw new IndexOutOfRangeException();
 			}
-			T[] newArr = new T[this.arr.Length - 1];
-			for (int i = 0; i < this.arr.Length - 1; i++)
+			TValue[] newValues = new TValue[this.values.Length - 1];
+			TKey[] newKeys = new TKey[this.keys.Length - 1];
+			for (int i = 0; i < this.keys.Length - 1; i++)
 			{
-				if (i >= iToRemove)
+				if (i >= iToRm)
 				{
-					newArr[i] = this.arr[i + 1];
+					newValues[i] = this.values[i + 1];
+					newKeys[i] = this.keys[i + 1];
 				}
 				else
 				{
-					newArr[i] = this.arr[i];
+					newValues[i] = this.values[i];
+					newKeys[i] = this.keys[i];
 				}
 			}
-			this.arr = newArr;
-			return this.arr.Length;
+			this.keys = newKeys;
+			this.values = newValues;
+			return this.keys.Length;
 		}
-		public T this[int index]
+		public TValue this[TKey key]
 		{
 			get
 			{
-				return this.arr[index];
-			}
-			set
-			{
-				if (this.arr.Length <= index)
+				int iToGet = -1;
+				for (int i = 0; i < this.keys.Length; i++)
+				{
+					if (this.keys[i].Equals(key))
+					{
+						iToGet = i;
+					}
+				}
+				if (iToGet >= this.keys.Length)
 				{
 					throw new IndexOutOfRangeException();
 				}
-				this.arr[index] = value;
+				return this.values[iToGet];
+			}
+			set
+			{
+				int iToSet = -1;
+				for (int i = 0; i < this.keys.Length; i++)
+				{
+					if (this.keys[i].Equals(key))
+					{
+						iToSet = i;
+					}
+				}
+				if (iToSet >= this.keys.Length)
+				{
+					throw new IndexOutOfRangeException();
+				}
+				this.values[iToSet] = value;
 			}
 		}
-		public T Get(int iToGet)
+		public TValue Get(TKey key)
 		{
-			if (this.arr.Length <= iToGet)
+			int iToGet = -1;
+			for (int i = 0; i < this.keys.Length; i++)
+			{
+				if (this.keys[i].Equals(key))
+				{
+					iToGet = i;
+				}
+			}
+			if (iToGet == -1)
 			{
 				throw new IndexOutOfRangeException();
 			}
-			return this.arr[iToGet];
+			return this.values[iToGet];
 		}
-		public int Add(T itemToAdd)
+		public int Add(TKey key, TValue value)
 		{
-			T[] temp = new T[this.arr.Length + 1];
-			for (int i = 0; i < this.arr.Length; i++)
+			bool hasThisKey = false;
+			for (int i = 0; i < this.keys.Length; i++)
 			{
-				temp[i] = this.arr[i];
+				if (this.keys[i].Equals(key))
+				{
+					hasThisKey = true;
+				}
 			}
-			temp[arr.Length] = itemToAdd;
-			this.arr = temp;
-			return this.arr.Length;
-		}
-		public int AddRange(MyList<T> lst)
-		{
-			T[] temp = new T[this.arr.Length + lst.Count];
-			for (int i = 0; i < temp.Length; i++)
+			if (hasThisKey)
 			{
-				if (i < this.arr.Length)
-				{
-					temp[i] = this.arr[i];
-				}
-				else
-				{
-					temp[i] = lst[i - this.arr.Length];
-				}
-
+				throw new Exception("Попытка добавить существующий ключ");
 			}
-			this.arr = temp;
-			return this.arr.Length;
+			TValue[] newValues = new TValue[this.values.Length + 1];
+			TKey[] newKeys = new TKey[this.keys.Length + 1];
+			for (int i = 0; i < this.keys.Length; i++)
+			{
+				newValues[i] = this.values[i];
+				newKeys[i] = this.keys[i];
+			}
+			newValues[newValues.Length - 1] = value;
+			newKeys[newKeys.Length - 1] = key;
+			this.keys = newKeys;
+			this.values = newValues;
+			return this.keys.Length;
 		}
 		public void Show()
 		{
-			foreach (var item in this.arr)
+			foreach (var item in this.values)
 			{
 				System.Console.WriteLine(item);
 			}
